90fabb86e4fc601cf3ea3c03c26c6c1a1647b5f3
Braeden Audlin <braedenaudlin@Braedens-MacBook-Pro.local>
2023-12-05 21:27:09 -0500

    Initial commit

---
+++ src/main/java/net/debtz/debtzplayground/DebtzPlayground.java
@@ -0,0 +1,15 @@
+package net.debtz.debtzplayground;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DebtzPlayground implements ModInitializer {
+	public static final String MOD_ID = "debtzplayground";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/debtz/debtzplayground/DebtzPlaygroundClient.java
@@ -0,0 +1,10 @@
+package net.debtz.debtzplayground;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DebtzPlaygroundClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
